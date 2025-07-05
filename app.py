9c8bb22012ebde956113814df7b4e787f9473b68
timshen_dev <timshen.dev@gmail.com>
2025-07-05 22:45:20 +0900

    Clean up tracked files using new .gitignore

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
+# request：用于读取请求里的参数（如 URL 中的 symbol=xxx）
+# jsonify：把 Python 字典/列表转成 JSON 响应，返回给前端
 from flask_cors import CORS
+# 当前端（如浏览器里 localhost:3000）请求你这个后端（如 localhost:5000）时，会因为域名/端口不同被浏览器阻止。加上 CORS 就可以放行。
 from data_fetcher import fetch_stock_data
 
 app = Flask(__name__)
 CORS(app)  # 允许前端跨域请求
 
-@app.route('/api/stock')
+@app.route('/api/stock') # 定义一个 API 路由，前端可以通过这个路由来获取股票数据
 def get_stock():
     symbol = request.args.get("symbol")   # 获取 URL 中的参数，如 AAPL
     if not symbol:
--- data_fetcher.py
+++ data_fetcher.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 def fetch_stock_data(symbol: str, period: str = "30d", interval: str = "1d"):
     try:
-        ticker = yf.Ticker(symbol)
-        df = ticker.history(period=period, interval=interval)
-        df.reset_index(inplace=True)         # 把时间列恢复成普通列
-        df["Date"] = df["Date"].astype(str)  # 把时间变成字符串，便于返回 JSON
+        ticker = yf.Ticker(symbol)  #创建 Ticker 对象 ，这是 yfinance 提供的类，用于操作某支股票。
+        df = ticker.history(period=period, interval=interval) # 调用 yfinance.Ticker 对象的 .history() 方法，从 Yahoo Finance 拉取历史股价数据。
+        df.reset_index(inplace=True)         # 把原本是索引的 Date 时间列，转换成普通的一列。
+        df["Date"] = df["Date"].astype(str)  # 把 Date 列的数据类型从 datetime 转换为 str（字符串），便于返回 JSON
         return df[["Date", "Close"]].to_dict(orient="records")
     except Exception as e:
         print(f"Error fetching data for {symbol}: {e}")
